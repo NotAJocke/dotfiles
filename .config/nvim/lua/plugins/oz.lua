99f52b1d5af5174e80ed94b1c430665bf1c0e156
NotAJocke <jockerider199@gmail.com>
2024-04-17 14:57:14 +0200

    yay deez nuts

--- .config/nvim/lua/plugins/oz.lua
+++ .config/nvim/lua/plugins/oz.lua
@@ -1,6 +1,22 @@
 return {
   "luxluth/oz.nvim",
+  lazy = false,
   opts = {
     ozengine_path = "/Applications/Mozart2.app/Contents/Resources/bin/ozengine",
+    keymaps = {
+      feed_selection_mapping = "<leader>os",
+    },
+  },
+  keys = {
+    {
+      "<leader>of",
+      "<cmd>OzFeedFile<CR>",
+      desc = "Execute oz file",
+    },
+    {
+      "<leader>ol",
+      "<cmd>OzOpenLog<CR>",
+      desc = "Open Oz logs",
+    },
   },
 }
