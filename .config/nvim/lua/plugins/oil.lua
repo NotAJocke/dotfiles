5f6ce0c6b05c56d0ffb6faf97cf803662704d6ee
NotAJocke <jockerider199@gmail.com>
2024-05-26 23:56:16 +0200

    deeeeeeeeeeeeeeez

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,11 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
+  opts = {
+    keymaps = {
+      ["H"] = "actions.parent",
+      ["L"] = "actions.select",
+    },
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
     { "<leader>e", "<CMD>Oil<CR>", desc = "Open parent directory" },
