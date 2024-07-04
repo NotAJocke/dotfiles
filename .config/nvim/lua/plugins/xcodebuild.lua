5b4ea7588dc0098784d38527fbc0cad5cde26258
NotAJocke <jockerider199@gmail.com>
2024-07-04 22:54:15 +0200

    fuck

---
+++ .config/nvim/lua/plugins/xcodebuild.lua
@@ -0,0 +1,10 @@
+return {
+  "wojciech-kulik/xcodebuild.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("xcodebuild").setup({})
+  end,
+}
